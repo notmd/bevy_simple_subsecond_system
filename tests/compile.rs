41524a334d248bf0309da9f9a91bf45ae91225ad
Jan Hohenheim <jan@hohenheim.ch>
2025-05-21 18:48:38 +0200

    Add test

--- tests/compile.rs
+++ tests/compile.rs
@@ -2,11 +2,40 @@
 use bevy::prelude::*;
 use bevy_simple_subsecond_system::prelude::*;
 
+#[test]
+fn add_to_app() {
+    App::new().add_systems(
+        Update,
+        (
+            empty_system,
+            system_with_commands,
+            system_with_commands_mut,
+            system_with_zst_query,
+            system_with_readonly_query,
+            system_with_mut_query,
+            system_with_mixed_query,
+            system_with_single_query,
+            system_with_resource,
+            system_with_resource_and_query,
+            system_with_mut_resource,
+            system_with_mut_resource_and_query,
+            system_with_mut_resource_and_mut_query,
+            system_with_mut_resource_and_single_query,
+            system_with_mut_resource_and_mut_single_query,
+            system_with_mut_resource_and_mut_single_query_rerun_true,
+            system_with_mut_resource_and_mut_single_query_rerun_false,
+        ),
+    );
+}
+
 #[hot]
 fn empty_system() {}
 
 #[hot]
 fn system_with_commands(commands: Commands) {}
+
+#[hot]
+fn system_with_commands_mut(mut commands: Commands) {}
 
 #[hot]
 fn system_with_zst_query(query: Query<()>) {}
